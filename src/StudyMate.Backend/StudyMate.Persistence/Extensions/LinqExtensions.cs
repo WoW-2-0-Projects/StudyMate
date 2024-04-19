d07b40ecb4e37e9a9fd5a62ff3e4b33a57d19430
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2024-04-20 01:17:31 +0500

    created linq extensions

---
+++ src/StudyMate.Backend/StudyMate.Persistence/Extensions/LinqExtensions.cs
@@ -0,0 +1,25 @@
+﻿using StudyMate.Domain.Common.Queries;
+
+namespace StudyMate.Persistence.Extensions;
+
+/// <summary>
+/// Provides extension methods for applying query specifications to IQueryable and IEnumerable collections.
+/// </summary>
+public static class LinqExtensions
+{
+    /// <summary>
+    /// Applies pagination to and IQeryable data source.
+    /// </summary>
+    /// <typeparam name="TSource">The type of elements in the data source.</typeparam>
+    /// <param name="sources">The IQueyable source to paginate.</param>
+    /// <param name="filterPagination">Pagination options such as page token and page size.</param>
+    /// <returns>An IQueryable representing the paginated subset of the oeiginal data source.</returns>
+    public static IQueryable<TSource> ApplyPagination<TSource>(
+        this IQueryable<TSource> sources,
+        FilterPagination filterPagination)
+    {
+        return sources
+            .Skip((int)((filterPagination.PageToken - 1) * filterPagination.PageSize))
+            .Take((int)filterPagination.PageSize);
+    }
+}
