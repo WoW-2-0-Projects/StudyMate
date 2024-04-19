0f4f2c350591289578992b1c1c10afa5c7f2a7fd
Husanboy <samadovxusan9013@gmail.com>
2024-04-20 00:21:07 +0500

    Create domain entities

---
+++ src/StudyMate.Backend/StudyMate.Domain/Common/Entities/IDelationAuditableEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace StudyMate.Domain.Common.Entities;
+
+/// <summary>
+/// Interface that marks an entity as being auditable for deletion tracking.
+/// </summary>
+public interface IDelationAuditableEntity
+{
+    /// <summary>
+    /// Gets or sets the ID of the user who deleted the entity.
+    /// </summary>
+    Guid? DeletedByUserId { get; set; }
+}
