53fa886a7c0db9598b52b78dfc49d48852acaec2
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2024-04-20 01:17:17 +0500

    created query options

---
+++ src/StudyMate.Backend/StudyMate.Domain/Common/Queries/QueryOptions.cs
@@ -0,0 +1,15 @@
+﻿namespace StudyMate.Domain.Common.Queries;
+
+/// <summary>
+/// Represents a set of options to configure data querying behavior.
+/// </summary>
+public struct QueryOptions
+{
+    /// <summary>
+    /// Gets or sets change tracking mode for query results.
+    /// Using AsNoTracking change tracking mode can potentially improve the performance.
+    /// </summary>
+    public QueryTrackingMode QueryTrackingMode { get; set; }
+
+    public QueryOptions(QueryTrackingMode queryTrackingMode) : this() => QueryTrackingMode = queryTrackingMode;
+}
---
+++ src/StudyMate.Backend/StudyMate.Domain/Common/Queries/QueryTrackingMode.cs
@@ -0,0 +1,22 @@
+﻿namespace StudyMate.Domain.Common.Queries;
+
+/// <summary>
+/// Represents different query tracking modes that can used while querying
+/// </summary>
+public enum QueryTrackingMode
+{
+    /// <summary>
+    /// Spceifies that queries should track changes to entities.
+    /// </summary>
+    AsTracking,
+
+    /// <summary>
+    /// Spceifies that queries should not track changes to entities.
+    /// </summary>
+    AsNoTracking,
+
+    /// <summary>
+    /// Spceifies that queries should not track changes to entities, but identity resolution is still performed.
+    /// </summary>
+    AsNoTrackingWithIdentityResolution
+}
