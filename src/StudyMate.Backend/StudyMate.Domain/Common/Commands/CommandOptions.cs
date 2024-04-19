01d51f0a2551e5d63f9a6999982670f89c2b562c
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2024-04-20 01:16:50 +0500

    created command options

---
+++ src/StudyMate.Backend/StudyMate.Domain/Common/Commands/CommandOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace StudyMate.Domain.Common.Commands;
+
+/// <summary>
+/// Represents options related to the execution of a command, particularly controlling data persistence.
+/// </summary>
+public struct CommandOptions()
+{
+    /// <summary>
+    /// Gets or sets a value indication whether changes made by the command should be automatically saved to the underlying data store.
+    /// </summary>
+    public bool SkipSaveChanges { get; set; } = false;
+}
