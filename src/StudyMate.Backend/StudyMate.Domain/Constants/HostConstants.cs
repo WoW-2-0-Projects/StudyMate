9d055a32d01a69398c2dcc8a3a31c2f3b57d6ffd
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2024-04-20 00:49:26 +0500

    Added CORS configurations

---
+++ src/StudyMate.Backend/StudyMate.Domain/Constants/HostConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace StudyMate.Domain.Constants;
+
+/// <summary>
+/// Represents host constants
+/// </summary>
+public static class HostConstants
+{
+    /// <summary>
+    /// CORS settings name constant
+    /// </summary>
+    public const string AllowSpecificOrigins = "AllowSpecificOrigins";
+}
