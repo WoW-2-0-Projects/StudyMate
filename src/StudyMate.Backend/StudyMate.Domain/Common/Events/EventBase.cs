48ed3f948469850240d56038e6dba2dfa204235c
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2024-04-20 13:00:35 +0500

    created event base and handler

---
+++ src/StudyMate.Backend/StudyMate.Domain/Common/Events/EventBase.cs
@@ -0,0 +1,22 @@
+﻿namespace StudyMate.Domain.Common.Events;
+
+/// <summary>
+/// Represents an implementation of `IEvent`, defining the properties for an event.
+/// </summary>
+public record EventBase : IEvent
+{
+    /// <summary>
+    /// Gets or sets the unique identifier of the event.
+    /// </summary>
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    /// <summary>
+    /// Gets or sets the date and time when the event was created.
+    /// </summary>
+    public DateTimeOffset CreatedTime { get; set; } = DateTimeOffset.UtcNow;
+
+    /// <summary>
+    /// Gets or sets a flag indicating whether the evnet has been redelivered.
+    /// </summary>
+    public bool Redelivered { get; set; }
+}
---
+++ src/StudyMate.Backend/StudyMate.Domain/Common/Events/EventHandlerBase.cs
@@ -0,0 +1,25 @@
+﻿using MassTransit;
+using MediatR;
+
+namespace StudyMate.Domain.Common.Events;
+
+/// <summary>
+/// Represents base for event handlers
+/// </summary>
+/// <typeparam name="TEvent"></typeparam>
+public abstract class EventHandlerBase<TEvent> : IEventHandler<TEvent> where TEvent : class, INotification
+{
+    public async Task Consume(ConsumeContext<TEvent> context) =>
+    await HandleAsync(context.Message, context.CancellationToken);
+
+    public async Task Handle(TEvent notification, CancellationToken cancellationToken) =>
+    await HandleAsync(notification, cancellationToken);
+
+    /// <summary>
+    /// Internal handle method that can be called by any event bus.
+    /// </summary>
+    /// <param name="event">The event to handle.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns></returns>
+    protected abstract ValueTask HandleAsync(TEvent @event, CancellationToken cancellationToken);
+}
