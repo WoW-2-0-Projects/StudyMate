ef74802151e957ac34c233a4bd899bed0666bc05
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2024-04-20 18:21:55 +0500

    Add true false question and question relation migration

--- src/StudyMate.Backend/StudyMate.Persistence/Migrations/20240420132040_Add_TrueFalseQuestion_And_Question.Designer.cs
+++ src/StudyMate.Backend/StudyMate.Persistence/Migrations/20240420132040_Add_TrueFalseQuestion_And_Question.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using StudyMate.Persistence.DataContexts;
@@ -12,9 +13,11 @@ using StudyMate.Persistence.DataContexts;
 namespace StudyMate.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240420132040_Add_TrueFalseQuestion_And_Question")]
+    partial class Add_TrueFalseQuestion_And_Question
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
