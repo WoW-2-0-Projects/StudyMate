76c19872827ea49b01abb0ff1a54918c4a84ad86
isxoqovxasanboy <isxoqovxasanboy1@gmail.com>
2024-04-20 09:51:25 +0500

    Created cache entity

---
+++ src/StudyMate.Backend/StudyMate.Domain/Common/Caching/ICacheEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace StudyMate.Domain.Common.Caching;
+
+/// <summary>
+/// Defines cache entry properties.
+/// </summary>
+public interface ICacheEntry
+{
+    /// <summary>
+    /// Gets or sets the cache key.
+    /// </summary>
+    string CacheKey { get; }
+}
