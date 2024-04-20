7de047ff35cef98392ec2bacbcc9f42289ac77e4
حَبِيبَةُ بِنْتُ شُهْرَتِ <sattorovahabiba00@gmail.com>
2024-04-20 13:01:27 +0500

    Implemented mass transit and rabbit mq bus broker

---
+++ src/StudyMate.Backend/StudyMate.Infrastructure/Common/EventBus/Brokers/RabbitMqEventBusBroker.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using StudyMate.Application.Common.Events.Brokers;
+using StudyMate.Domain.Common.Events;
+
+namespace StudyMate.Infrastructure.Common.EventBus.Brokers;
+
+/// <summary>
+/// Implementation of <see cref="IEventBusBroker"/> that utilizes a mediator for publishing events.
+/// </summary>
+/// <param name="mediator">The mediator instance to be used for event publication.</param>
+public class RabbitMqEventBusBroker(IPublisher mediator) : IEventBusBroker
+{
+    public async ValueTask PublishLocalAsync<TEvent>(TEvent @event, CancellationToken cancellationToken = default) where TEvent : EventBase =>
+        await mediator.Publish(@event, cancellationToken);
+
+    public async ValueTask PublishAsync<TEvent>(TEvent @event, EventOptions eventOptions, CancellationToken cancellationToken = default) where TEvent : EventBase =>
+        await mediator.Publish(@event, cancellationToken);
+}
