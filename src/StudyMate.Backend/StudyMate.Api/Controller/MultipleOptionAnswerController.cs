309e31bf22af3d114b6786316fd2cea1834412eb
Husanboy <samadovxusan9013@gmail.com>
2024-04-20 16:16:57 +0500

    Add controller

---
+++ src/StudyMate.Backend/StudyMate.Api/Controller/MultipleOptionAnswerController.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using StudyMate.Application.MultipleOptionAnswers.Queries;
+using StudyMate.Application.MultipleOptionAnswers.Service;
+
+namespace StudyMate.Api.Controller;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MultipleOptionAnswerController(
+    IMediator mediator,
+    IMapper mapper,
+    IMultipleOptionAnswerService multipleOptionAnswerService) : ControllerBase
+{
+    [HttpPost]
+    public async ValueTask<IActionResult> Get([FromQuery] GetMultipleOptionAnswerQuery query,
+                                              CancellationToken cancellationToken)
+    {
+        var result = await mediator.Send(query, cancellationToken);
+        return result.Any() ? Ok(result) : NoContent();
+    }
+}
