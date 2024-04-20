2d57d71d03812c2804e852eff7a86180109bfd61
Husanboy <samadovxusan9013@gmail.com>
2024-04-20 16:16:43 +0500

    Add query and queryhandler

---
+++ src/StudyMate.Backend/StudyMate.Application/MultipleOptionAnswers/Queries/GetMultipleOptionAnswerQuery.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Input;
+using StudyMate.Application.MultipleOptionAnswers.Models;
+using StudyMate.Domain.Common.Queries;
+
+namespace StudyMate.Application.MultipleOptionAnswers.Queries;
+
+public class GetMultipleOptionAnswerQuery:IQuery<ICollection<MultipleOptionAnswerDto>>
+{
+    public MultipleOptionAnswerFilterModel FilterModel { get; set; } = default!;
+}
